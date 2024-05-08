fb701ad8a977adf441b93462f26e350ab46cd92f
barcaca <73846225+barcaca@users.noreply.github.com>
2024-05-08 14:04:35 -0300

    Exemplos Estrturas Condicionais

---
+++ estruturas-condicionais/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    for (int carneirinos = 1; carneirinos <= 20; carneirinos++) {
+      System.out.println(carneirinos + " carneirinos");
+    }
+
+  }
+}
