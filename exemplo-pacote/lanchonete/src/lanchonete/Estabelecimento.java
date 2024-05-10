d02e7c0d1d562bfc66f7501846399e600a17e7d9
barcaca <73846225+barcaca@users.noreply.github.com>
2024-05-10 15:43:18 -0300

    Mais Exemplos POO

--- exemplo-pacote/lanchonete/src/lanchonete/Estabelecimento.java
+++ exemplo-pacote/lanchonete/src/lanchonete/Estabelecimento.java
@@ -7,16 +7,16 @@ import lanchonete.atendimento.cozinha.Cozinheiro;
 public class Estabelecimento {
 	public static void main(String[] args) {
 		Cozinheiro cozinheiro = new Cozinheiro();
-		
-		//ações que estabelecimento precisa ter ciência
+
+		// ações que estabelecimento precisa ter ciência
 		cozinheiro.adicionarSucoNoBalcao();;
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
-		
+
 		Atendente atendente = new Atendente();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
-		
+
 		Cliente cliente = new Cliente();
 		cliente.escolherLanche();
 		cliente.fazerPedido();
