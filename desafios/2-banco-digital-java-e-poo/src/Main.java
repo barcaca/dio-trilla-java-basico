7039b8371b5ec255a9c7b232407dd8c6e6b5b5c7
barcaca <73846225+barcaca@users.noreply.github.com>
2024-05-17 11:47:58 -0300

    Desafio Banco Digital

---
+++ desafios/2-banco-digital-java-e-poo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    Cliente luan = new Cliente();
+    luan.setNome("Luan");
+    Conta cc = new ContaCorrente(luan);
+    Conta cp = new ContaPoupanca(luan);
+    cc.depositar(100);
+		cc.transferir(100, cp);
+		
+		cc.imprimirExtrato();
+		cp.imprimirExtrato();
+  }
+}
